5f6017cb47f64158c1fbb8c23fc16495d288b765
aidact3 <aidacarabot.tosar@gmail.com>
2024-07-08 21:40:14 -0400

    fixing vite images

--- components/data/data.js
+++ components/data/data.js
@@ -1,8 +1,8 @@
 const data = {
   name: "Aida Carabot",
   title: "Full-Stack Developer & E-Commerce Manager",
-  avatar:"./public/assets/cv_image.jpg",
-  cv:"./public/assets/cv/AidaCarabot_CV.pdf",
+  avatar:"/assets/cv_image.jpg",
+  cv:"/assets/cv/AidaCarabot_CV.pdf",
   aboutMe:
     "Passionate web developer with a diverse background in e-commerce, marketing, and content creation, seeking a challenging tech role. My journey began with a Bachelor's in Business Administration, where I developed a strong foundation in business principles and analytical thinking. Driven by an insatiable curiosity for coding and a knack for crafting user-centric web applications, I embarked on a Full-Stack Development Bootcamp (currently finishing Back-End).",
 
@@ -23,7 +23,7 @@ const data = {
       graduationYear: 2024,
       location: "Online",
       description: "Driven by a long-standing passion for programming, I decided to transition into tech by enrolling in an intensive Full-Stack Development Bootcamp. Recognizing the transformative power of technology in today's business landscape, I am driven to bridge the gap between my solid business foundation and the dynamic world of software development. I have successfully completed the Front-End segment and am currently finalizing the Back-End portion. Recognizing the transformative power of technology in today's business landscape, I am driven to bridge the gap between my solid business foundation and the dynamic world of software development.",
-      logo: "./public/assets/thePower.jpeg",
+      logo: "/assets/thePower.jpeg",
   },
   {
     degree: "Nova Talent Member",
@@ -31,7 +31,7 @@ const data = {
     graduationYear: 2023,
     location: "Madrid, ES",
     description: "Nova is the merit-based access network where the top 3% of talent connect with each other, develop and accelerate their careers. This elite community connects me with visionary leaders, grants access to exclusive events, and provides mentorship from seasoned experts – all fueling my career growth and positioning me for success in the fast-paced business and tech landscape.",
-    logo: "./public/assets/nova.jpeg",
+    logo: "/assets/nova.jpeg",
   },
   {
     degree: "Bachelor of Business Administration (English Degree)",
@@ -39,7 +39,7 @@ const data = {
     graduationYear: 2022,
     location: "Madrid, ES",
     description: "This university is one of the most prestigious and internationally recognized in the field. With a diverse and highly qualified faculty, UC3M ensures students receive top-tier education and mentorship. Graduates are known for their strong analytical skills, global mindset, and strategic decision-making abilities, making them highly sought after by employers worldwide.",
-    logo: "./public/assets/uc3m.png",
+    logo: "/assets/uc3m.png",
   },
   {
     degree: "BBA Exchange year program",
@@ -47,7 +47,7 @@ const data = {
     graduationYear: 2022,
     location: "Paris, FR",
     description: "Paris School of Business instills in its students the values and skills necessary to become effective and socially responsible managers. During my Erasmus year at this renowned institution, I gained a comprehensive understanding of business principles while developing a deep appreciation for the ethical and societal responsibilities of business leaders. This transformative experience has prepared me to navigate the global business landscape with confidence and make a positive impact on the world.",
-    logo: "./public/assets/PSB.jpeg",
+    logo: "/assets/PSB.jpeg",
   },
   ],
 
@@ -61,7 +61,7 @@ const data = {
       location: "Dominican Republic (Remote)",
       description:
         "Worked for an Start-up which is an official Amazon Service Provider Network certified partner, delivering Amazon brand strategy, full-service brand management, and scalable advertising solutions to drive long-term, profitable brand growth. I developed long-lasting client relationships by providing tailored services focused on increasing sales on Amazon, optimizing Amazon Advertising campaigns, and enhancing inventory and catalog management. Additionally, I conducted in-depth market and competitor analysis to help clients surpass their competition, create effective deal strategies to boost sales and market presence, and led cross-functional teams to ensure high-quality work to achieve client objectives.",
-      logo: "./public/assets/ecomcy.jpeg",
+      logo: "./assets/ecomcy.jpeg",
     },
     {
       position: "Brand Specialist (Retail E-Commerce Manager) - EU level",
@@ -71,7 +71,7 @@ const data = {
       location: "Madrid, ES",
       description:
         "Drove initiatives to enhance top KPIs (i.e., traffic, availability, conversion rate, and profitability) for leading strategic brands at the EU level. Coordinated Tier 1 Deal Events (i.e., Prime Day, Black Friday), leading negotiations and ensuring optimal outcomes. Established key internal partnerships with various retail functions, contributing to the definition of priorities and operational goals tailored to each brand's specific needs. Coordinated a European project aimed at revamping the main landing page of the category to improve the overall customer experience.",
-      logo: "./public/assets/amazon.jpeg",
+      logo: "/assets/amazon.jpeg",
     },
     {
       position: "Operations Internship",
@@ -81,7 +81,7 @@ const data = {
       location: "Madrid, ES",
       description:
         "Monitored and optimized order processing to ensure the timely and accurate shipment of orders. Analyzed operational data to identify trends, areas for improvement, and key performance indicators. Led teams to enhance the efficiency of shipment processing. Collaborated with key stakeholders in planning and developing project launches to optimize operational timings.",
-      logo: "./public/assets/amazon.jpeg",
+      logo: "/assets/amazon.jpeg",
     },
     {
       position: "Internet Safety Evaluator",
@@ -91,7 +91,7 @@ const data = {
       location: "Ireland (Remote)",
       description:
         "Evaluated online content by reviewing and assessing websites, social media platforms, and other online content to ensure compliance with safety standards and guidelines. Collaborated with cross-functional teams, including legal, engineering, and customer support, to address internet safety issues and implement effective solutions.",
-      logo: "./public/assets/telus.jpeg",
+      logo: "/assets/telus.jpeg",
     },
     {
       position: "Community Manager",
@@ -101,7 +101,7 @@ const data = {
       location: "Madrid, ES",
       description:
         "Developed and executed content marketing strategies across social media platforms to engage and resonate with the community. Drove initiatives to increase brand awareness within the community, fostering a stronger connection between the brand and its audience. Analyzed and reported on community metrics to measure engagement, track growth, and assess the impact of community-building efforts. Developed and curated engaging and relevant content for various social media channels, aligning with brand identity and community interests.",
-      logo: "./public/assets/aupair.jpeg",
+      logo: "/assets/aupair.jpeg",
     },
   ],
   skills: {
@@ -109,35 +109,35 @@ const data = {
     usingNow : [
         {
           skill: "HTML5",
-          logo: "./public/skills/html5.png",
+          logo: "/skills/html5.png",
         },
         {
           skill: "CSS3",
-          logo: "./public/skills/css3.png",
+          logo: "/skills/css3.png",
         },
         {
           skill: "JAVASCRIPT",
-          logo: "./public/skills/js.png",
+          logo: "/skills/js.png",
         },
         {
           skill: "SCSS / SASS",
-          logo: "./public/skills/sass.png",
+          logo: "/skills/sass.png",
         },
         {
           skill: "GIT",
-          logo: "./public/skills/git.png",
+          logo: "/skills/git.png",
         },
         {
           skill: "GITHUB",
-          logo: "./public/skills/github.png",
+          logo: "/skills/github.png",
         },
         {
           skill: "FIGMA",
-          logo: "./public/skills/figma.png",
+          logo: "/skills/figma.png",
         },
         {
           skill: "VITE",
-          logo: "./public/skills/vite.png",
+          logo: "/skills/vite.png",
         },
       
       ],
@@ -145,35 +145,35 @@ const data = {
       learning : [
         {
           skill: "NODEJS",
-          logo: "./public/skills/nodejs.png",
+          logo: "/skills/nodejs.png",
         },
         {
           skill: "MONGODB",
-          logo: "./public/skills/mongodb.png",
+          logo: "/skills/mongodb.png",
         },
         {
           skill: "REACT",
-          logo: "./public/skills/react.png",
+          logo: "/skills/react.png",
         },
         {
           skill: "TYPESCRIPT",
-          logo: "./public/skills/typescript.png",
+          logo: "/skills/typescript.png",
         },
         {
           skill: "PYTHON",
-          logo: "./public/skills/python.jpg",
+          logo: "/skills/python.jpg",
         },
         {
           skill: "ANGULAR",
-          logo: "./public/skills/angular.png",
+          logo: "/skills/angular.png",
         },
         {
           skill: "NEXTJS",
-          logo: "./public/skills/nextjs.png",
+          logo: "/skills/nextjs.png",
         },
         {
           skill: "MySQL",
-          logo: "./public/skills/mysql.webp",
+          logo: "/skills/mysql.webp",
         },
       ]
     ,
@@ -181,35 +181,35 @@ const data = {
       otherSkills : [
         {
           skill: "ENGLISH (C1/C2)",
-          logo: "./public/skills/english.png",
+          logo: "/skills/english.png",
         },
         {
           skill: "FRENCH (B2/C1)",
-          logo: "./public/skills/french.png",
+          logo: "/skills/french.png",
         },
         {
           skill: "SPANISH (NATIVE)",
-          logo: "./public/skills/spanish.png",
+          logo: "/skills/spanish.png",
         },
         {
           skill: "AMAZON RETAIL",
-          logo: "./public/skills/amazon-retail.png",
+          logo: "/skills/amazon-retail.png",
         },
         {
           skill: "DIGITAL MARKETING",
-          logo: "./public/skills/digital-marketing.png",
+          logo: "/skills/digital-marketing.png",
         },
         {
           skill: "E-COMMERCE MANAGEMENT",
-          logo: "./public/skills/e-commerce.png",
+          logo: "/skills/e-commerce.png",
         },
         {
           skill: "LEADERSHIP",
-          logo: "./public/skills/leadership.png",
+          logo: "/skills/leadership.png",
         },
         {
           skill: "PROBLEM-SOLVING",
-          logo: "./public/skills/business.png",
+          logo: "/skills/business.png",
         },
       ]
     ,
@@ -225,7 +225,7 @@ const data = {
       link: "https://pinterest-api.netlify.app/",
       repository: "https://github.com/aidact3/Project-3_PinterestAPI",
       preview:
-        "./public/project/pinterest.png",
+        "/project/pinterest.png",
     },
     {
       title: "E-Commerce Website - iZone",
